c023875f7f3d038feb404ef4952e3e959d143384
Samiylo Kryshu <skryshu@gmail.com>
2020-04-07 19:20:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,9 +19,10 @@ class Transfer
   
   def execute_transaction
     if @sender.balance > @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
+      if @sender.valid? && @receiver.valid?
+        @sender.balance -= @amount
+        @receiver.balance += @amount
+        @status = "complete"
     else
       @status = "rejected"
       "Transaction rejected. Please check your account balance."
